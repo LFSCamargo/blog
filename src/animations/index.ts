4abff493ec1a2d4a3f3b371f826577a1198ffc50
LFSCamargo <luizepauloxd@gmail.com>
2021-07-12 12:56:49 -0300

    feat: adds all lottie animations that will be used

---
+++ src/animations/index.ts
@@ -0,0 +1,5 @@
+/** All the Lottie animations will be stored in here  */
+export { default as Articles } from './articles.json';
+export { default as ContactUs } from './contact-us.json';
+export { default as Projects } from './projects.json';
+export { default as Error404 } from './404.json';
