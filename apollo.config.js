bf6e8483c3f5e591bfe0b4c169eec70f78aa2eab
LFSCamargo <luizepauloxd@gmail.com>
2021-07-12 12:54:57 -0300

    chore: apollo config

---
+++ apollo.config.js
@@ -0,0 +1,14 @@
+/**  @description - This will provide autocomplete for queries */
+module.exports = {
+  client: {
+    service: {
+      name: 'blog',
+      localSchemaFile: './graphql/schema.graphql',
+      url: 'https://graphql.contentful.com/content/v1/spaces/bniasprfxkbv',
+      headers: {
+        authorization: `Bearer d6raGW3ZfoqiGceLeu2062uz96vOxuhiTenILoSZA2w`
+      },
+      skipSSLValidation: true
+    }
+  }
+};
